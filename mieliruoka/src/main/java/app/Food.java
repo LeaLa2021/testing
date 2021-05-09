6c5c4b286602fdb861fba15a182e484344b44176
tuiretuominen <58442768+tuiretuominen@users.noreply.github.com>
2021-05-09 17:32:53 +0300

    add text to Food.java

--- mieliruoka/src/main/java/app/Food.java
+++ mieliruoka/src/main/java/app/Food.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.io.IOException;
+
 import java.io.PrintWriter;
 
 import javax.servlet.annotation.WebServlet;
@@ -8,3 +9,4 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.io.Serializable;
